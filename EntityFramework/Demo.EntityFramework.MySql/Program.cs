eabf1a378d6794b056880e4631ba043bb020fbe8
penCsharpener <matthiasmueller@protonmail.com>
2019-07-14 15:44:28 +0200

    Demo.EntityFramework.MySql: setup project and solution

---
+++ EntityFramework/Demo.EntityFramework.MySql/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Demo.EntityFramework.MySql {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
